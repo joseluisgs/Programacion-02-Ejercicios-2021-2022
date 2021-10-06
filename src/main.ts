c4be9aa4751874881484958dee9424514d6e62f0
JoaquinAyllon <joaquinayllongar@gmail.com>
2021-10-06 20:44:33 +0200

    optimización y arreglo de bogs

--- src/main.ts
+++ src/main.ts
@@ -29,46 +29,47 @@ while (keepPlay && money > 0) {
         console.log("Tu carta es: " + mod.cardToPlayer(cardStorer));
         sum = sum + mod.cardToGame(cardStorer);
 
-    } while (wantMore && sum <= 7.5) {
-        console.log("Quieres más cartas? ");
-        wantMore = read.keyInYNStrict("");
-        if (wantMore) {
-            cardStorer = mod.randomCardGenerator();
-            console.log("Tu carta es: " + mod.cardToPlayer(cardStorer));
-            sum = sum + mod.cardToGame(cardStorer);
+        while (wantMore && sum <= 7.5) {
+            console.log("Quieres más cartas? ");
+            wantMore = read.keyInYNStrict("");
+            if (wantMore) {
+                cardStorer = mod.randomCardGenerator();
+                console.log("Tu carta es: " + mod.cardToPlayer(cardStorer));
+                sum = sum + mod.cardToGame(cardStorer);
+            }
         }
-    }
-    if (sum > 7.5) {
-        console.log("Has superado los 7.5, has perdido " + bet + " euros")
-        money = money - bet;
-    } else {
-        console.log("Tu jugada es de: " + sum)
-
-        while (sum > bankSum && bankSum <= 7.5) {
-            cardStorer = mod.randomCardGenerator();
-            bankSum = bankSum + mod.cardToGame(cardStorer)
-            console.log("La banca roba carta y recibe un: " + mod.cardToPlayer(cardStorer))
-
-        }
-        if (sum > bankSum || bankSum > 7.5) {
-            console.log("Tu jugada es de: " + sum)
-            console.log("La de la banca es de: " + bankSum)
-            console.log("Has ganado " + bet + " euros")
-            money = money + bet;
-
-        } else if (sum <= bankSum) {
-
-            console.log("Tu jugada es de: " + sum)
-            console.log("La de la banca es de: " + bankSum)
-            console.log("Has perdido " + bet + " euros")
+        if (sum > 7.5) {
+            console.log("Has superado los 7.5, has perdido " + bet + " euros");
             money = money - bet;
+        } else {
+            console.log("Tu jugada es de: " + sum)
+
+            while (sum > bankSum && bankSum <= 7.5) {
+                cardStorer = mod.randomCardGenerator();
+                bankSum = bankSum + mod.cardToGame(cardStorer);
+                console.log("La banca roba carta y recibe un: " + mod.cardToPlayer(cardStorer));
+
+            }
+            if (sum > bankSum || bankSum > 7.5) {
+                console.log("Tu jugada es de: " + sum);
+                console.log("La de la banca es de: " + bankSum);
+                console.log("Has ganado " + bet + " euros");
+                money = money + bet;
+
+            } else if (sum <= bankSum) {
+
+                console.log("Tu jugada es de: " + sum);
+                console.log("La de la banca es de: " + bankSum);
+                console.log("Has perdido " + bet + " euros");
+                money = money - bet;
+            }
         }
     }
 }
-console.log("El juego ha terminado")
-console.log("Has acabado con: " + money)
-if (money > initialMoney){
-    console.log("Tu ganancia es de: "+ (money-initialMoney))
+console.log("El juego ha terminado");
+console.log("Has acabado con: " + money);
+if (money > initialMoney) {
+    console.log("Tu ganancia es de: " + (money - initialMoney));
 } else {
-    console.log("Tu perdida es de: "+ (initialMoney-money))
+    console.log("Tu perdida es de: " + (initialMoney - money));
 }
